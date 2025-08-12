291b91d178f73bce310e2a44dd1fa1f0496991f1
csili2025 <csili1414@gmail.com>
2025-08-12 16:44:12 +0300

    GCG-02: As a developer, I want a PostgreSQL schema so that data persists reliably. - 1 half of the postgres schema migrations done

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,9 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('oauth_provider')->nullable();
+            $table->integer('xp')->default(0);
             $table->timestamps();
         });
 
