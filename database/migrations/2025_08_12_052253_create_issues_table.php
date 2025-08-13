1a8ecf84090b58b3ef809dac0e204618abcf37e8
Pap Noemi <papnoemi199999@gmail.com>
2025-08-13 15:12:27 +0300

    GCG-02: As a developer, I want a PostgreSQL schema so that data persists reliably. - fix: add issue url in migraton: add title in bounty factory

--- database/migrations/2025_08_12_052253_create_issues_table.php
+++ database/migrations/2025_08_12_052253_create_issues_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('repo_id')->constrained('repos')->cascadeOnDelete();
             $table->text('description')->nullable();
+            $table->string('url');
             $table->timestamps();
         });
     }
