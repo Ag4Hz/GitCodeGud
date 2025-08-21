a9ca4937e810eae7047e17fcab39f47a8394a75a
Pap Noemi <papnoemi199999@gmail.com>
2025-08-21 13:44:17 +0300

    GCG-13: As a user, I want to search friends by GitHub username so that I can connect.
    add: controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,27 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class DashboardController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return inertia('Dashboard');
-    }
+        $users = User::select(['id', 'name', 'nickname'])
+            ->orderBy('nickname')
+            ->paginate(10);
 
-    public function searchUsers(Request $request)
-    {
-        return User::query()
-            ->when($request->search, fn ($q) =>
-            $q->where('nickname', 'like', "%{$request->search}%")
-            )
-            ->paginate(30)
-            ->withQueryString()
-            ->through(fn ($user) => [
-                'id'       => $user->id,
-                'nickname' => $user->nickname,
-                'avatar'   => $user->avatar,
-                'name'     => $user->name,
-            ]);
+        return Inertia::render('Profile', [
+            'users' => $users,
+        ]);
     }
 }
