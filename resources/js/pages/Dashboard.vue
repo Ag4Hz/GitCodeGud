acd37a385fabb20fc10a0863e6c897a540f5a879
Pap Noemi <papnoemi199999@gmail.com>
2025-08-21 22:45:02 +0300

    GCG-13: As a user, I want to search friends by GitHub username so that I can connect.
    add: placeholder

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -47,6 +47,7 @@ const breadcrumbs: BreadcrumbItem[] = [{ title: 'Dashboard', href: '/dashboard' 
                             :value="search"
                             @input="(e: InputEvent) => (search = (e.target as HTMLInputElement).value)"
                             class="w-full rounded-md border px-12 py-2 focus:border-green-600 focus:ring-2 focus:ring-green-600 focus:outline-none"
+                            placeholder="Looking for a buddy?"
                         />
                     </div>
 
