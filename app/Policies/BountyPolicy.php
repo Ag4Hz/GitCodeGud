64c908b7fc41a4aaab8af95defff1e2ce5d16cdb
csili2025 <csili1414@gmail.com>
2025-08-21 09:58:46 +0300

    GCG-17: As a user, I want to create a bounty so that others can work on it. - BountyManagement

--- app/Policies/BountyPolicy.php
+++ app/Policies/BountyPolicy.php
@@ -1,1 +1,36 @@
 <?php
+namespace App\Policies;
+
+use App\Models\Bounty;
+use App\Models\User;
+class BountyPolicy
+{
+    public function viewAny(User $user):bool
+    {
+        return true;
+    }
+    public function view(User $user,Bounty $bounty):bool
+    {
+        return true;
+    }
+    public function create(User $user):bool
+    {
+        return true;
+    }
+    public function update(User $user,Bounty $bounty):bool
+    {
+        return $bounty->issue && $bounty->issue->repo && $bounty->issue->repo->user_id === $user->id;
+    }
+    public function delete(User $user,Bounty $bounty):bool
+    {
+        return $bounty->issue && $bounty->issue->repo && $bounty->issue->repo->user_id === $user->id;
+    }
+    public function restore(User $user, Bounty $bounty): bool
+    {
+        return $this->delete($user, $bounty);
+    }
+    public function forceDelete(User $user,Bounty $bounty):bool
+    {
+        return $this->delete($user, $bounty);
+    }
+}
