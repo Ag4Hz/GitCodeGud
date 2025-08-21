382775ad2e46a035e50438bdab80bc4c8f1821c6
Pap Noemi <papnoemi199999@gmail.com>
2025-08-21 17:51:46 +0300

    GCG-13: As a user, I want to search friends by GitHub username so that I can connect.
    change: switch to combobox

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,23 +9,12 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        return inertia('Dashboard');
-    }
-
-    public function searchUsers(Request $request)
-    {
-        return User::query()
-            ->when($request->search, fn ($q) =>
-            $q->where('nickname', 'like', "%{$request->search}%")
-            )
-            ->paginate(30)
-            ->withQueryString()
-            ->through(fn ($user) => [
-                'id'       => $user->id,
-                'nickname' => $user->nickname,
-                'avatar'   => $user->avatar,
-                'name'     => $user->name,
-            ]);
+        return Inertia::render('Dashboard', [
+            'users' => User::paginate(10)->through(fn($user) => [
+                'id' => $user->id,
+                'name' => $user->name
+            ])
+        ]);
     }
 
 }
