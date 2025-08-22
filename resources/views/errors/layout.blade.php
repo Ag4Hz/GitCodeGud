08814c571c6f6b553059fd0c3a25b5a8dd4fcd52
Pap Noemi <papnoemi199999@gmail.com>
2025-08-22 15:45:18 +0300

    GCG-41: As a user of the application, I want to see custom-designed error pages when something goes wrong.
    remove: footer
    add: display image on error page dark/light

--- resources/views/errors/layout.blade.php
+++ resources/views/errors/layout.blade.php
@@ -26,29 +26,18 @@
                 <p class="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8 dark:text-gray-400">Sorry, we
                     couldn’t find the page you’re looking for.</p>
                 <div class="mt-10">
-                    <a href="/{{ route('dashboard') }}" class="text-sm/7 font-semibold text-indigo-600 dark:text-indigo-400"><span
+                    <a href="/dashboard" class="text-sm/7 font-semibold text-indigo-600 dark:text-indigo-400"><span
                             aria-hidden="true">&larr;</span> Back to home</a>
                 </div>
             </div>
         </main>
-        <footer class="self-end lg:col-span-2 lg:col-start-1 lg:row-start-3">
-            <div class="border-t border-gray-100 bg-gray-50 py-10 dark:border-white/10 dark:bg-gray-800/50">
-                <nav
-                    class="mx-auto flex w-full max-w-7xl items-center gap-x-4 px-6 text-sm/7 text-gray-600 lg:px-8 dark:text-gray-400">
-                    <a href="#">Contact support</a>
-                    <svg viewBox="0 0 2 2" aria-hidden="true" class="size-0.5 fill-gray-300 dark:fill-gray-600">
-                        <circle r="1" cx="1" cy="1" />
-                    </svg>
-                    <a href="#">Status</a>
-                </nav>
-            </div>
-        </footer>
+
         <div class="hidden lg:relative lg:col-start-2 lg:row-start-1 lg:row-end-4 lg:block">
             <img
-                src="https://images.unsplash.com/photo-1583585635793-0e1894c169bd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1825&q=80"
+                src="/assets/images/not-found-dark.png"
                 alt="" class="absolute inset-0 size-full object-cover not-dark:hidden" />
             <img
-                src="https://images.unsplash.com/photo-1470847355775-e0e3c35a9a2c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1825&q=80"
+                src="/assets/images/not-found.png"
                 alt="" class="absolute inset-0 size-full object-cover dark:hidden" />
         </div>
     </div>
