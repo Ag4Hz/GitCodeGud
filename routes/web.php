bfbdffa29ad2a81985a8168e61a6606b83b74732
Ágoston Denezsi <agostondenezsi@mail.com>
2025-08-25 13:10:00 +0300

    GCG-38: As an admin, I want to configure XP rules so that rewards are tunable.
    - created blank 'Admin' page
    - created middleware for checking if the user has admin privileges
    - created route to the 'Admin' page

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,12 @@ Route::middleware(['auth', 'verified'])->group(function () {
         ->name('bounties.restore');
 });
 
+Route::middleware(['auth', 'verified', 'admin'])->group(function () {
+    Route::get('/admin', function () {
+        return Inertia::render('Admin');
+    })->name('admin');
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/leaderboard', [LeaderboardController::class, 'index'])
         ->name('leaderboard');
