e1b50a1730a7044b9d92da668c578b62d1cad003
csili2025 <csili1414@gmail.com>
2025-08-26 20:07:07 +0300

    BountyController

--- app/Services/UserBountyService.php
+++ app/Services/UserBountyService.php
@@ -18,13 +18,4 @@ class UserBountyService
             ->orderBy('created_at', 'desc')
             ->paginate($perPage);
     }
-    public function getUserActiveBounties(User $user, int $perPage = 10): LengthAwarePaginator
-    {
-        return Bounty::with(['issue.repo', 'submissions'])
-            ->whereHas('issue.repo', function ($query) use ($user) {
-                $query->where('user_id', $user->id);
-            })
-            ->orderBy('created_at', 'desc')
-            ->paginate($perPage);
-    }
 }
