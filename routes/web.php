79deab27ab297eda647a2391a7a35d0b4b741aef
csili2025 <csili1414@gmail.com>
2025-08-26 16:10:06 +0300

    web

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,9 @@ Route::get('/', function () {
     return Inertia::render('Welcome');
 })->name('home');
 
-Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('dashboard', function () {
+    return Inertia::render('Dashboard', []);
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
