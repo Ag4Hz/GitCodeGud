2d2a9aadf76febb941f230ce16a0b7d0e85d945d
Ágoston Denezsi <agostondenezsi@mail.com>
2025-08-27 09:15:12 +0300

    GCG-38: As an admin, I want to configure XP rules so that rewards are tunable.
    - migration bugfix

--- database/migrations/2025_08_27_052039_create_general_settings.php
+++ database/migrations/2025_08_27_052039_create_general_settings.php
@@ -8,7 +8,7 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('xp_settings', function (Blueprint $table) {
+        Schema::create('general_settings', function (Blueprint $table) {
             $table->id();
             $table->string('key')->unique();
             $table->string('value');
