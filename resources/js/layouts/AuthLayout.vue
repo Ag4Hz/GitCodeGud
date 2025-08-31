9cdbfa5160bd3c47d2143d71be21b5eed475e724
Pap Noemi <papnoemi199999@gmail.com>
2025-08-31 23:18:58 +0300

    GCG-42: As a visitor I want a simple Log in page with only “Continue with GitHub” So that I know exactly how to sign in.
    add: login card

--- resources/js/layouts/AuthLayout.vue
+++ resources/js/layouts/AuthLayout.vue
@@ -8,7 +8,7 @@ defineProps<{
 </script>
 
 <template>
-    <AuthLayout :title="title" :description="description">
+    <AuthLayout :title="title" :description="description" class="bg-transparent" >
         <slot />
     </AuthLayout>
 </template>
