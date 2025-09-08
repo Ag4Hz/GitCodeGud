ea396d37d01c8f12aaf34fd00b77f3772fcde9df
csili2025 <csili1414@gmail.com>
2025-09-08 14:57:08 +0300

    seeders

--- database/seeders/SubmissionSeeder.php
+++ database/seeders/SubmissionSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Bounty;
 use App\Models\Submission;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,17 +15,32 @@ class SubmissionSeeder extends Seeder
      */
     public function run(): void
     {
-         $bounties = Bounty::all();
+        $bounties = Bounty::with('issue.repo')->get();
 
         if ($bounties->isEmpty()) {
-            $this->call(BountySeeder::class);
-            $bounties = Bounty::all();
+            $this->command->info('No bounties found. Please run BountySeeder first.');
+            return;
+        }
+
+        $users = User::all();
+
+        if ($users->isEmpty()) {
+            $this->command->info('No users found. Creating some users first.');
+            $users = User::factory(10)->create();
         }
 
         foreach ($bounties as $bounty) {
-            Submission::factory(rand(1, 3))->create([
-                'bounty_id' => $bounty->id
-            ]);
+            if ($bounty->submissions()->count() === 0) {
+                $submissionCount = rand(1, 3);
+                $selectedUsers = $users->random(min($submissionCount, $users->count()));
+
+                foreach ($selectedUsers as $user) {
+                    Submission::factory()->create([
+                        'bounty_id' => $bounty->id,
+                        'user_id' => $user->id,
+                    ]);
+                }
+            }
         }
     }
 }
