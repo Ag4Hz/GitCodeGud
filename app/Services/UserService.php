4ee0ee56f97f2295ea86d293839ddfd3b2741381
csili2025 <csili1414@gmail.com>
2025-11-22 11:18:09 +0200

    GCG-52: As a developer, I want dynamic OAuth controllers so that GitHub/GitLab/Bitbucket login works.  - Service (username/nickname))

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -9,9 +9,18 @@ class UserService
     public static function listUser(string $term = '', ?int $page = 1, ?int $perPage = 30): LengthAwarePaginator
     {
         return User::query()
-            ->when($term, fn($query) => $query->where('nickname', 'ILIKE', "%{$term}%"))
+            ->when($term, function ($query) use ($term) {
+                $query->where(function ($q) use ($term) {
+                    // Search by nickname
+                    $q->where('nickname', 'ILIKE', "%{$term}%")
+                        // OR search by any provider username (GitHub, GitLab, Bitbucket)
+                        ->orWhereHas('providers', function ($q) use ($term) {
+                            $q->where('provider_username', 'ILIKE', "%{$term}%");
+                        });
+                });
+            })
+            ->with('providers')
             ->orderByRaw('LOWER(nickname)')
-
             ->orderBy('nickname')
             ->paginate(20, ['*'], 'users_page', $page ?? 1)
             ->withQueryString()
@@ -20,6 +29,10 @@ class UserService
                 'nickname' => $user->nickname,
                 'avatar' => $user->avatar,
                 'name' => $user->name,
+                'providers' => $user->providers->map(fn ($p) => [
+                    'provider' => $p->provider,
+                    'provider_username' => $p->provider_username,
+                ])->values(),
             ]);
     }
 
