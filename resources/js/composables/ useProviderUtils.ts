289ff2576940be60fccebb113954b0f814153f39
csili2025 <csili1414@gmail.com>
2025-12-08 17:03:13 +0200

    GCG-63: As a user, I want to see provider info on bounty list and detail pages. - useProviderUtils with  svg icon + Show

--- resources/js/composables/ useProviderUtils.ts
+++ resources/js/composables/ useProviderUtils.ts
@@ -1,4 +1,3 @@
-import { Github, GitlabIcon as Gitlab } from 'lucide-vue-next';
 import type { Component } from 'vue';
 
 export interface ProviderConfig {
@@ -18,7 +17,7 @@ export function useProviderUtils() {
             case 'github':
                 return {
                     name: 'GitHub',
-                    icon: Github,
+                    icon: GithubIcon,
                     color: 'text-gray-800 dark:text-gray-200',
                     borderColor: 'border-gray-600 dark:border-gray-400',
                     badgeColor: 'bg-gray-100 text-gray-800 dark:bg-gray-700 dark:text-gray-200',
@@ -27,7 +26,7 @@ export function useProviderUtils() {
             case 'gitlab':
                 return {
                     name: 'GitLab',
-                    icon: Gitlab,
+                    icon: GitlabIcon,
                     color: 'text-orange-600 dark:text-orange-400',
                     borderColor: 'border-orange-500 dark:border-orange-400',
                     badgeColor: 'bg-orange-100 text-orange-800 dark:bg-orange-900/30 dark:text-orange-300',
@@ -36,7 +35,7 @@ export function useProviderUtils() {
             case 'bitbucket':
                 return {
                     name: 'Bitbucket',
-                    icon: Bitbucket,
+                    icon: BitbucketIcon,
                     color: 'text-blue-600 dark:text-blue-400',
                     borderColor: 'border-blue-500 dark:border-blue-400',
                     badgeColor: 'bg-blue-100 text-blue-800 dark:bg-blue-900/30 dark:text-blue-300',
@@ -45,7 +44,7 @@ export function useProviderUtils() {
             default:
                 return {
                     name: provider ? provider.charAt(0).toUpperCase() + provider.slice(1) : 'Git',
-                    icon: Github,
+                    icon: GithubIcon,
                     color: 'text-gray-600 dark:text-gray-400',
                     borderColor: 'border-gray-400 dark:border-gray-500',
                     badgeColor: 'bg-gray-100 text-gray-700 dark:bg-gray-800 dark:text-gray-300',
@@ -107,10 +106,26 @@ export function useProviderUtils() {
     };
 }
 
-const Bitbucket = {
+const GithubIcon: Component = {
     template: `
         <svg viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-            <path d="M2.654 3.004A1.51 1.51 0 001.15 4.48l2.776 16.456a2.052 2.052 0 002.02 1.72h12.11a1.513 1.513 0 001.487-1.246L22.35 4.48a1.51 1.51 0 00-1.505-1.476zm10.794 12.972H9.47l-1.123-6.36h5.804z"/>
+            <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
+        </svg>
+    `,
+};
+
+const GitlabIcon: Component = {
+    template: `
+        <svg viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path d="M2.4 3A1.3 1.3 0 0 0 1.1 4.5l2.7 15.9c.1.5.6.9 1.2.9h13a1.3 1.3 0 0 0 1.2-1.1l2.7-15.7A1.3 1.3 0 0 0 20.7 3H2.4zm9.6 12.3H9.3l-.9-6.6h7.2l-.9 6.6h-2.7z"/>
+        </svg>
+    `,
+};
+
+const BitbucketIcon: Component = {
+    template: `
+        <svg viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path d="M2.39 9.73L12 22l9.61-12.27a.7.7 0 0 0-.25-.97L19.07 7 16.7 1.27a.7.7 0 0 0-1.32 0L12 7.33 8.62 1.27a.7.7 0 0 0-1.32 0L4.93 7 2.64 8.76a.7.7 0 0 0-.25.97Z"/>
         </svg>
     `,
 };
