b28f8d53f9df52c88c538e3c0fea2096cc59a7bc
csili2025 <csili1414@gmail.com>
2025-12-27 18:49:03 +0200

    GCG-74: As a QE, I want user search and discovery tests so that GCG-68 functionality is verified. - FollowerTest

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,12 +2,12 @@
 
 namespace Tests\Unit;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseTruncation;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    use RefreshDatabase;
+    use DatabaseTruncation;
 
     public function test_that_true_is_true()
     {
