d378c4c52c0682c8b6411c3bbe0905d050d5a570
csili2025 <csili1414@gmail.com>
2026-05-19 19:21:13 +0300

    navbar and submission fixing mobil view

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -6,7 +6,7 @@ import AppLogoIcon from '@/components/AppLogoIcon.vue';
     <div class="flex aspect-square size-10 lg:size-15 items-center justify-center rounded-md p-1 ...">
         <AppLogoIcon class="size-4 lg:size-5 fill-current text-white dark:text-black" />
     </div>
-    <div class="ml-1 grid flex-1 text-left text-sm">
-        <img src="@/../assets/title.svg" alt="Logo title" class="mt-3 h-7 lg:h-10 w-auto" />
+    <div class="ml-1 grid text-left text-sm">
+        <img src="@/../assets/title.svg" alt="Logo title" class="mt-3 h-7 lg:h-10 w-auto max-w-[80px] lg:max-w-[120px]" />
     </div>
 </template>
--- resources/js/components/LeaderboardTable.vue
+++ resources/js/components/LeaderboardTable.vue
@@ -53,7 +53,7 @@ const displayXP = (user: User) => (showLevel.value ? formatXP(user.skill_xp ?? 0
         <img
             src="/assets/images/peek.png"
             alt=""
-            class="absolute -top-25 left-1/2 z-50 h-32 w-32 -translate-x-1/2 drop-shadow-[0_-10px_15px_rgba(255,255,255,0.1)] sm:scale-100"
+            class="absolute -top-3 left-1/2 z-50 w-[12vw] min-w-[80px] max-w-[128px] h-auto -translate-x-1/2 -translate-y-[68%] drop-shadow-[0_-10px_15px_rgba(255,255,255,0.1)]"
         />
 
         <div class="z-0 mx-auto w-full max-w-7xl overflow-x-auto rounded-2xl border border-gray-200 bg-white/40 dark:border-white/10 dark:bg-white/5">
